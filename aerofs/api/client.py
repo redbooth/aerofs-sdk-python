d66a7c3a6f53f93fa8c12b9672fb431e279d74d6
Kevin James <KevinJames@thekev.in>
2016-01-06 09:51:42 -0800

    Cleaned up API implementation

--- aerofs/api/client.py
+++ aerofs/api/client.py
@@ -8,156 +8,175 @@ class APIClient(object):
     def __init__(self, instance_configuration, access_token):
         self.instance_configuration = instance_configuration
         self.access_token = access_token
-        self.session = requests.Session()
-        self.session.headers = self._auth_headers()
 
-    def _url_prefix(self):
-        return 'https://{}{}'.format(self.instance_configuration.hostname,
-                                     VERSION_PREFIX)
-
-    def _auth_headers(self):
-        headers = {
-            'Authorization': 'Bearer {}'.format(self.access_token),
-            'Endpoint-Consistency': 'strict',
+        self.auth_headers = {
+            'Authorization': 'Bearer {}'.format(access_token),
             'Content-Type': 'application/json',
+            'Endpoint-Consistency': 'strict',
         }
-        return headers
+        self.url_prefix = 'https://{}{}'.format(
+            instance_configuration.hostname, VERSION_PREFIX)
+
+        self.session = requests.Session()
+
+        self.response_headers = None
+
+    # requests
+
+    def _handle_response(self, response):
+        response.raise_for_status()
+        self.response_headers = response.headers
+        return response.json()
+
+    def _do_delete(self, route, headers=None):
+        if not headers:
+            headers = self.auth_headers
+
+        res = self.session.delete('{}{}'.format(self.url_prefix, route),
+                                  headers=headers)
+        return self._handle_response(res)
+
+    def _do_get(self, route, headers=None):
+        if not headers:
+            headers = self.auth_headers
+
+        res = self.session.get('{}{}'.format(self.url_prefix, route),
+                               headers=headers)
+        return self._handle_response(res)
+
+    def _do_post(self, route, data, headers=None):
+        if not headers:
+            headers = self.auth_headers
+
+        res = self.session.post('{}{}'.format(self.url_prefix, route),
+                                json=data, headers=headers)
+        return self._handle_response(res)
+
+    def _do_put(self, route, data, headers=None):
+        if not headers:
+            headers = self.auth_headers
+
+        res = self.session.put('{}{}'.format(self.url_prefix, route),
+                               json=data, headers=headers)
+        return self._handle_response(res)
 
     # user object
 
     def get_user(self, email):
-        url = '{}/users/{}'.format(self._url_prefix(), email)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/users/{}'.format(email)
+        return self._do_get(route)
 
     def create_user(self, email, first_name, last_name):
+        route = '/users'
         data = {'email': email, 'first_name': first_name,
                 'last_name': last_name}
-
-        url = '{}/users'.format(self._url_prefix())
-        res = self.session.post(url, json=data)
-        res.raise_for_status()
-        return res.json()
+        return self._do_post(route, data)
 
     def update_user(self, email, first_name, last_name):
+        route = '/users/{}'.format(email)
         data = {'email': email, 'first_name': first_name,
                 'last_name': last_name}
-
-        url = '{}/users/{}'.format(self._url_prefix(), email)
-        res = self.session.put(url, json=data)
-        res.raise_for_status()
-        return res.json()
-
-    def update_user_password(self, email, password):
-        data = '"{}"'.format(password)
-
-        url = '{}/users/{}/password'.format(self._url_prefix(), email)
-        res = self.session.put(url, data=data)
-        res.raise_for_status()
-        return 'OK'
+        return self._do_put(route, data)
 
     def delete_user(self, email):
-        url = '{}/users/{}'.format(self._url_prefix(), email)
-        res = self.session.delete(url)
-        res.raise_for_status()
-        return 'OK'
+        route = '/users/{}'.format(email)
+        return self._do_delete(route)
+
+    def update_user_password(self, email, password):
+        route = '/users/{}/password'.format(email)
+        data = '"{}"'.format(password)
+        return self._do_put(route, data)
 
     def delete_user_password(self, email):
-        url = '{}/users/{}/password'.format(self._url_prefix(), email)
-        res = self.session.delete(url)
-        res.raise_for_status()
-        return 'OK'
+        route = '/users/{}/password'.format(email)
+        return self._do_delete(route)
 
     # folder object
 
-    def get_folder(self, uuid):
-        url = '{}/folders/{}'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+    def get_folder(self, uuid, fields=None):
+        route = '/folders/{}'.format(uuid)
+        if fields:
+            route += '?fields={}'.format(','.join(fields))
+        return self._do_get(route)
 
     def get_folder_path(self, uuid):
-        url = '{}/folders/{}/path'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/folders/{}/path'.format(uuid)
+        return self._do_get(route)
 
     def get_folder_children(self, uuid):
-        url = '{}/folders/{}/children'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/folders/{}/children'.format(uuid)
+        return self._do_get(route)
 
     def create_folder(self, parent_folder, foldername):
+        route = '/folders'
         data = {'parent': parent_folder, 'name': foldername}
+        return self._do_post(route, data)
 
-        url = '{}/folders'.format(self._url_prefix())
-        res = self.session.post(url, json=data)
-        res.raise_for_status()
-        return res.json()
+    def move_folder(self, uuid, parent_folder, foldername, ifmatch=None):
+        headers = self.auth_headers
+        if ifmatch:
+            headers['If-Match'] = ifmatch
 
-    def move_folder(self, uuid, parent_folder, foldername):
+        route = '/folders/{}'.format(uuid)
         data = {'parent': parent_folder, 'name': foldername}
+        return self._do_put(route, data, headers=headers)
 
-        url = '{}/folders/{}'.format(self._url_prefix(), uuid)
-        res = self.session.put(url, json=data)
-        res.raise_for_status()
-        return res.json()
+    def delete_folder(self, uuid, ifmatch=None):
+        headers = self.auth_headers
+        if ifmatch:
+            headers['If-Match'] = ifmatch
 
-    def delete_folder(self, uuid):
-        url = '{}/folders/{}'.format(self._url_prefix(), uuid)
-        res = self.session.delete(url)
-        res.raise_for_status()
-        return 'OK'
+        route = '/folders/{}'.format(uuid)
+        return self._do_delete(route, headers=headers)
 
     # file object
 
-    def get_file(self, uuid):
-        url = '{}/files/{}'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+    def get_file(self, uuid, fields=None):
+        route = '/files/{}'.format(uuid)
+        if fields:
+            route += '?fields={}'.format(','.join(fields))
+        return self._do_get(route)
 
     def get_file_path(self, uuid):
-        url = '{}/files/{}/path'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/files/{}/path'.format(uuid)
+        return self._do_get(route)
 
-    def get_file_content(self, uuid):
-        url = '{}/files/{}/content'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+    def get_file_content(self, uuid, ranges=None, ifrange=None,
+                         ifnonematch=None):
+        headers = self.auth_headers
+        if ranges:
+            headers['Range'] = ','.join(ranges)
+        if ifrange:
+            headers['If-Range'] = ifrange
+        if ifnonematch:
+            headers['If-None-Match'] = ','.join(ifnonematch)
+
+        route = '/files/{}/content'.format(uuid)
+        return self._do_get(route, headers=headers)
 
     def create_file(self, parent_folder, filename):
+        route = '/files'
         data = {'parent': parent_folder, 'name': filename}
+        return self._do_post(route, data)
 
-        url = '{}/files'.format(self._url_prefix())
-        res = self.session.post(url, json=data)
-        res.raise_for_status()
-        return res.json()
-
-    def upload_file_content(self, uuid, stream):
-        url = '{}/files/{}/content'.format(self._url_prefix(), uuid)
+    def upload_file_content(self, uuid, stream, ifmatch=None):
+        route = '/files/{}/content'.format(uuid)
 
         # Create upload identifier.
-        initial_headers = {
+        headers = {
             'Content-Range': 'bytes */*',
             'Content-Length': '0',
         }
+        if ifmatch:
+            headers['If-Match'] = ','.join(ifmatch)
 
-        res = self.session.put(url, headers=initial_headers)
-        res.raise_for_status()
-
-        upload_id = res.headers['Upload-ID']
-        etag = res.headers.get('ETag')
-        current_chunk = stream.read(MAX_CHUNK_SIZE)
-        total_bytes_sent = 0
+        self._do_put(route, dict(), headers=headers)
+        upload_id = self.response_headers['Upload-ID']
+        etag = self.response_headers.get('ETag')
 
         # Upload content, one chunk at a time.
-        headers = None
+        current_chunk = stream.read(MAX_CHUNK_SIZE)
+        total_bytes_sent = 0
         while len(current_chunk) != 0:
             headers = {
                 'Upload-ID': upload_id,
@@ -166,157 +185,146 @@ class APIClient(object):
                     total_bytes_sent,
                     total_bytes_sent + len(current_chunk) - 1),
             }
-
             if etag:
                 headers['If-Match'] = etag
 
-            res = self.session.put(url, headers=headers,
-                                   data=io.BytesIO(current_chunk))
-            res.raise_for_status()
+            self._do_put(route, io.BytesIO(current_chunk), headers=headers)
 
             total_bytes_sent += len(current_chunk)
             current_chunk = stream.read(MAX_CHUNK_SIZE)
 
         # Commit upload.
-        commit_headers = {
+        headers = {
             'Upload-ID': upload_id,
             'Endpoint-Consistency': 'strict',
             'Content-Range': 'bytes */{}'.format(total_bytes_sent),
             'Content-Length': '0',
         }
-
         if etag:
             headers['If-Match'] = etag
 
-        res = self.session.put(url, headers=commit_headers)
-        res.raise_for_status()
-        return 'OK'
+        return self._do_put(route, dict(), headers=headers)
 
-    def move_file(self, uuid, parent_folder, filename):
+    def move_file(self, uuid, parent_folder, filename, ifmatch=None):
+        headers = self.auth_headers
+        if ifmatch:
+            headers['If-Match'] = ','.join(ifmatch)
+
+        route = '/files/{}'.format(uuid)
         data = {'parent': parent_folder, 'name': filename}
+        return self._do_put(route, data, headers=headers)
 
-        url = '{}/files/{}'.format(self._url_prefix(), uuid)
-        res = self.session.put(url, json=data)
-        res.raise_for_status()
-        return res.json()
+    def delete_file(self, uuid, ifmatch=None):
+        headers = self.auth_headers
+        if ifmatch:
+            headers['If-Match'] = ','.join(ifmatch)
 
-    def delete_file(self, uuid):
-        url = '{}/files/{}'.format(self._url_prefix(), uuid)
-        res = self.session.delete(url)
-        res.raise_for_status()
-        return 'OK'
+        route = '/files/{}'.format(uuid)
+        return self._do_delete(route, headers=headers)
 
     # shared folder object
 
-    def get_shared_folders(self, email):
-        url = '{}/users/{}/shares'.format(self._url_prefix(), email)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+    def get_shared_folders(self, email, ifnonematch=None):
+        headers = self.auth_headers
+        if ifnonematch:
+            headers['If-None-Match'] = ','.join(ifnonematch)
 
-    def get_shared_folder_metadata(self, uuid):
-        url = '{}/shares/{}'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/users/{}/shares'.format(email)
+        return self._do_get(route, headers=headers)
+
+    def get_shared_folder_metadata(self, uuid, ifnonematch=None):
+        headers = self.auth_headers
+        if ifnonematch:
+            headers['If-None-Match'] = ','.join(ifnonematch)
+
+        route = '/shares/{}'.format(uuid)
+        return self._do_get(route, headers=headers)
 
     def create_shared_folder(self, foldername):
+        route = '/shares'
         data = {'name': foldername}
-
-        url = '{}/shares'.format(self._url_prefix())
-        res = self.session.post(url, json=data)
-        res.raise_for_status()
-        return res.json()
+        return self._do_post(route, data)
 
     # member object
 
-    def get_members(self, uuid):
-        url = '{}/shares/{}/members'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+    def get_members(self, uuid, ifnonematch=None):
+        headers = self.auth_headers
+        if ifnonematch:
+            headers['If-None-Match'] = ','.join(ifnonematch)
 
-    def get_member(self, uuid, email):
-        url = '{}/shares/{}/members/{}'.format(self._url_prefix(), uuid, email)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/shares/{}/members'.format(uuid)
+        return self._do_get(route, headers=headers)
+
+    def get_member(self, uuid, email, ifnonematch=None):
+        headers = self.auth_headers
+        if ifnonematch:
+            headers['If-None-Match'] = ','.join(ifnonematch)
+
+        route = '/shares/{}/members/{}'.format(uuid, email)
+        return self._do_get(route, headers=headers)
 
     def add_member(self, uuid, email, permissions):
+        route = '/shares/{}/members'.format(uuid)
         data = {'email': email, 'permissions': permissions}
+        return self._do_post(route, data)
 
-        url = '{}/shares/{}/members'.format(self._url_prefix(), uuid)
-        res = self.session.post(url, json=data)
-        res.raise_for_status()
-        return res.json()
+    def update_member_permissions(self, uuid, email, permissions,
+                                  ifmatch=None):
+        headers = self.auth_headers
+        if ifmatch:
+            headers['If-Match'] = ','.join(ifmatch)
 
-    def update_member_permissions(self, uuid, email, permissions):
+        route = '/shares/{}/members/{}'.format(uuid, email)
         data = {'permissions': permissions}
+        return self._do_put(route, data, headers=headers)
 
-        url = '{}/shares/{}/members/{}'.format(self._url_prefix(), uuid, email)
-        res = self.session.put(url, json=data)
-        res.raise_for_status()
-        return res.json()
+    def remove_member(self, uuid, email, ifmatch=None):
+        headers = self.auth_headers
+        if ifmatch:
+            headers['If-Match'] = ','.join(ifmatch)
 
-    def remove_member(self, uuid, email):
-        url = '{}/shares/{}/members/{}'.format(self._url_prefix(), uuid, email)
-        res = self.session.delete(url)
-        res.raise_for_status()
-        return 'OK'
+        route = '/shares/{}/members/{}'.format(uuid, email)
+        return self._do_delete(route, headers=headers)
 
     # pending member object
 
-    def get_pending_members(self, uuid):
-        url = '{}/shares/{}/pending'.format(self._url_prefix(), uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+    def get_pending_members(self, uuid, ifnonematch=None):
+        headers = self.auth_headers
+        if ifnonematch:
+            headers['If-None-Match'] = ','.join(ifnonematch)
+
+        route = '/shares/{}/pending'.format(uuid)
+        return self._do_get(route, headers=headers)
 
     def get_pending_member(self, uuid, email):
-        url = '{}/shares/{}/pending/{}'.format(self._url_prefix(), uuid, email)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/shares/{}/pending/{}'.format(uuid, email)
+        return self._do_get(route)
 
     def add_pending_member(self, uuid, email, permissions, note):
+        route = '/shares/{}/pending'.format(uuid)
         data = {'email': email, 'permissions': permissions, 'note': note}
-
-        url = '{}/shares/{}/pending'.format(self._url_prefix(), uuid)
-        res = self.session.post(url, json=data)
-        res.raise_for_status()
-        return res.json()
+        return self._do_post(route, data)
 
     def remove_pending_member(self, uuid, email):
-        url = '{}/shares/{}/pending/{}'.format(self._url_prefix(), uuid, email)
-        res = self.session.delete(url)
-        res.raise_for_status()
-        return 'OK'
+        route = '/shares/{}/pending/{}'.format(uuid, email)
+        return self._do_delete(route)
 
     # invitation object
 
     def get_invitations(self, email):
-        url = '{}/users/{}/invitations'.format(self._url_prefix(), email)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/users/{}/invitations'.format(email)
+        return self._do_get(route)
 
     def get_invitation(self, email, uuid):
-        url = '{}/users/{}/invitations/{}'.format(self._url_prefix(), email,
-                                                  uuid)
-        res = self.session.get(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/users/{}/invitations/{}'.format(email, uuid)
+        return self._do_get(route)
 
-    def accept_invitation(self, email, uuid):
-        url = '{}/users/{}/invitations/{}'.format(self._url_prefix(), email,
-                                                  uuid)
-        res = self.session.post(url)
-        res.raise_for_status()
-        return res.json()
+    def accept_invitation(self, email, uuid, external=False):
+        route = '/users/{}/invitations/{}'.format(email, uuid)
+        if external:
+            route += '?external=1'
+        return self._do_post(route, dict())
 
     def ignore_invitation(self, email, uuid):
-        url = '{}/users/{}/invitations/{}'.format(self._url_prefix(), email,
-                                                  uuid)
-        res = self.session.delete(url)
-        res.raise_for_status()
-        return res.json()
+        route = '/users/{}/invitations/{}'.format(email, uuid)
+        return self._do_delete(route)
