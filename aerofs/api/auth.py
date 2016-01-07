281a70bc228367b27ac5ebb423209b117f2cdcc3
Kevin James <KevinJames@thekev.in>
2016-01-06 17:26:35 -0800

    add auth routes

--- aerofs/api/auth.py
+++ aerofs/api/auth.py
@@ -1,12 +1,46 @@
-#import requests
+import requests
+
 
 class APIAuthClient(object):
     def __init__(self, instance_configuration, app_credentials):
         self.instance_configuration = instance_configuration
         self.app_credentials = app_credentials
 
+        self.url_prefix = 'https://{}'.format(instance_configuration.hostname)
+
+        self.session = requests.Session()
+        self.session.headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+
+    def get_authorization_url(self, scopes):
+        scopes = ','.join(scopes)
+        return ('{}/authorize?response_type=code'
+                '&redirect_uri={}'
+                '&scope={}'
+                '&client_id={}'
+               ).format(self.url_prefix, self.app_credentials.redirect_uri,
+                        scopes, self.app_credentials.client_id)
+
     def get_access_token_with_code(self, code):
-        # TODO finish this...
-        # This is required to facilitate the full OAuth flow. Not required if
-        # we are using the user settings token.
-        return
+        route = '/auth/token'
+        data = '&'.join('{}={}'.format(k, v) for k, v in {
+            'client_id': self.app_credentials.client_id,
+            'client_secret': self.app_credentials.client_secret,
+            'redirect_uri': self.app_credentials.redirect_uri,
+            'grant_type': 'authorization_code',
+            'code': code,
+        }.iteritems())
+        res = self.session.post(
+            '{}{}'.format(self.url_prefix, route), data=data,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'})
+
+        try:
+            res.raise_for_status()
+        except requests.exceptions.HTTPError:
+            if res.json()['error'] == 'invalid_grant':
+                return None
+
+            raise
+
+        print res.json()['access_token']
--- aerofs/api/creds.py
+++ aerofs/api/creds.py
@@ -1,4 +1,5 @@
 class AppCredentials(object):
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id, client_secret, redirect_uri):
         self.client_id = client_id
         self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
